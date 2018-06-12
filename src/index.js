5d1ca363b413369f4e5665db61d045298bd04ef3
Dwire <gjdwyer8@gmail.com>
2018-06-12 15:49:37 -0400

    user login working. add sign up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,66 @@
+// // w/create-react-app
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./App";
+// import registerServiceWorker from "./registerServiceWorker";
+//
+// // added based on Nat
+// import { Provider } from "react-redux";
+// import { createStore, applyMiddleware } from "redux";
+// import userReducer from "./reducers/index";
+// import thunk from "redux-thunk"
+//
+// const store = createStore(userReducer, applyMiddleware(thunk));
+//
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById("root")
+// );
+// registerServiceWorker();
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import 'semantic-ui-css/semantic.min.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './App';
+
+const initialState = { currentUser: {} };
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SET_CURRENT_USER':
+      const { id, name } = action.user;
+      return { ...state, currentUser: { id, name } };
+    case 'LOGOUT_USER':
+      return { ...state, currentUser: {} };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(reduxThunk));
+
+const Root = ({ store }) => {
+  return (
+    <Router>
+      <Provider store={store}>
+        <Route path="/" component={App} />
+      </Provider>
+    </Router>
+  );
+};
+
+ReactDOM.render(<Root store={store} />, document.getElementById('root'));
---
+++ src/reducers/index.js
@@ -0,0 +1,3 @@
+export default function hobbitReducer(state = {user: []}, action) {
+
+}
